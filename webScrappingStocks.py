0493abf280a8aeac61464b1c84a81bd9214c41e0
PatoAh <98765285+vilmapato@users.noreply.github.com>
2025-02-10 15:36:50 +0100

    started webscrapping stocks

---
+++ webScrappingStocks.py
@@ -0,0 +1,5 @@
+import bs4 as bs
+import requests
+
+# Fetch info from url
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/amazon_data_webpage.html."
